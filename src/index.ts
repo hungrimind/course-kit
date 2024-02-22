0cd7fddd6dfc79115b283aa8595f705093033c0f
Tadas Petra <tadas@tadaspetra.com>
2024-02-22 13:12:25 -0600

    Added a test folder to make sure it's working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { Course } from "../src/components/Course";
+
+export type { CourseProps, HeaderSectionProps, MarkdownSectionProps, Section, SlideSectionProps } from "../src/components/Course";
+
