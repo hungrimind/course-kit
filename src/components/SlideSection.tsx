e7317a9e3919f715b1af41989311775c722e0db7
Tadas Petra <tadas@tadaspetra.com>
2024-02-26 11:34:59 -0600

    Currently uploaded

--- src/components/SlideSection.tsx
+++ src/components/SlideSection.tsx
@@ -1,4 +1,4 @@
-// import Prism from "prismjs";
+import Prism from "prismjs";
 import "prismjs/components/prism-dart";
 import "prismjs/components/prism-markdown";
 import "prismjs/components/prism-yaml";
@@ -18,12 +18,10 @@ interface SlideSectionPageProps {
 }
 
 const SlideSection: React.FC<SlideSectionPageProps> = (props) => {
-  // might want to listen to a specific state rather than doing it all the time
   useEffect(() => {
-    import("prismjs").then((Prism) => {
-      Prism.default.highlightAll();
-    });
-  }, []);
+    console.log("highlighting in package");
+    Prism.highlightAll();
+  });
 
   return (
     <div className="relative flex h-full w-full">
--- test-astro/src/component/TestComp.tsx
+++ test-astro/src/component/TestComp.tsx
@@ -1,21 +1,9 @@
 import { Course, type Section } from "course-kit";
 import "course-kit/dist/index.css";
-import Prism from "prismjs";
-import { useEffect } from "react";
 import test from "../test.json";
 
 const lessons = test.lessons as Section[];
 
-export default function Home(props: { lessons: Section[] }) {
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
-
-  return (
-    <main className="flex flex-col w-full">
-      <div className="w-screen h-screen">
-        <Course id={0} lessons={props.lessons} canExit={false} />
-      </div>
-    </main>
-  );
+export default function StupidCourse(props: { lessons: Section[] }) {
+  return <Course id={0} lessons={props.lessons} canExit={false} />;
 }
