e9f057500fe6037d15051c50d1afb88a029595c2
Tadas Petra <tadas@tadaspetra.com>
2024-02-27 15:11:57 -0600

    added watch, and moved button classes to global file.
    This is how we easily let people modify the UI to however they want, we set the default classes and then they can overwrite it if they want to.

--- test-astro/src/component/TestComp.tsx
+++ test-astro/src/component/TestComp.tsx
@@ -5,5 +5,11 @@ import test from "../test.json";
 const lessons = test.lessons as Section[];
 
 export default function StupidCourse(props: { lessons: Section[] }) {
-  return <Course id={0} lessons={props.lessons} canExit={false} />;
+  return (
+    <main className="flex flex-col w-full">
+      <div className="w-screen h-screen">
+        <Course id={0} lessons={props.lessons} canExit={false} />
+      </div>
+    </main>
+  );
 }
